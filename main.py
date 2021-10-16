6eea2b6bd694e578510249c8872728a5008243f4
Maria <m.v.friedel@gmail.com>
2021-10-16 14:13:15 +0200

    Changes by ruhkopf

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ import pandas as pd
 xlxs = pd.ExcelFile('data/Training_set_full.xlsx')
 
 # read out the sheets
-df1 = pd.read_excel(xlxs,'table_of_contents_en-2')
+df1 = pd.read_excel(xlxs, 'table_of_contents_en-2')
 df2 = pd.read_excel(xlxs, 'Training')
 
 # Join id from d1 to df2 by using join function
-# df.merge(df1,df2, how='right')
 mapping = pd.merge(df1, df2, on="indicator.label")
 
+# write out to csv for a closer look
 mapping.to_csv('data/mapping.csv')
 
 # relevant variables
 mapping[['id', 'Identification term']]
 
+# create mapping
+# https://www.geeksforgeeks.org/how-to-list-values-for-each-pandas-group/
+groups = mapping[['id', 'Identification term']].groupby('Identification term')['id'].apply(list)
+listgroups = groups.reset_index(name='listvalues') # series object: index: label, value: list of csv_ids
+
+# dictionary comprehension to assign every ID to an Identification term--> key value pair
+# label ---> csv_id
+label_to_id = {k: v for k, v in listgroups.values}
