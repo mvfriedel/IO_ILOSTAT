dbf043f5abdfcc7e22c1f03ef3ca4714669400ae
Maria <m.v.friedel@gmail.com>
2021-10-16 15:37:07 +0200

    Preprocessing for mappings saved to json format

--- src/preprocess_label_to_id.py
+++ src/preprocess_label_to_id.py
@@ -1,8 +1,9 @@
 # open the excelfile and two different tabs
+import json
 import pandas as pd
 
 # load the entire excel including all sheets
-xlxs = pd.ExcelFile('data/Training_set_full.xlsx')
+xlxs = pd.ExcelFile('../data/raw/Training_set_full.xlsx')
 
 # read out the sheets
 df1 = pd.read_excel(xlxs, 'table_of_contents_en-2')
@@ -12,7 +13,7 @@ df2 = pd.read_excel(xlxs, 'Training')
 mapping = pd.merge(df1, df2, on="indicator.label")
 
 # write out to csv for a closer look
-mapping.to_csv('data/mapping.csv')
+mapping.to_csv('../data/results/mapping.csv')
 
 # relevant variables
 mapping[['id', 'Identification term']]
@@ -25,3 +26,23 @@ listgroups = groups.reset_index(name='listvalues') # series object: index: label
 # dictionary comprehension to assign every ID to an Identification term--> key value pair
 # label ---> csv_id
 label_to_id = {k: v for k, v in listgroups.values}
+
+# Serializing json
+json_object = json.dumps(label_to_id, indent = 4)
+print(json_object)
+
+# json_object with hrefs:
+pattern = 'https://www.ilo.org/ilostat-files/WEB_bulk_download/indicator/{id}.csv.gz'
+label_to_id_href = {k: [pattern.format(id=v) for v in list_of_ids] for k, list_of_ids in label_to_id.items()}
+
+
+#writing the json file to with. 'With' is a context manager.
+with open("../data/results/IO_Labels.json", "w") as outfile:
+    json.dump(label_to_id, outfile)
+
+
+print()
+
+#writing the json file to with. 'With' is a context manager including hyperreference
+with open("../data/results/label_to_id_href.json", "w") as outfile:
+    json.dump(label_to_id_href, outfile)
--- src/preprocess_scrape.py
+++ src/preprocess_scrape.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import urllib
+import json
 from bs4 import BeautifulSoup
 
 # (0) get all the hyperlinks to the .csv.gz files
 URL = 'https://ilo.org/ilostat-files/WEB_bulk_download/html/bulk_indicator.html'
-overview = pd.read_html(URL) # links are not present here so we need to parse the actual webpage
+overview = pd.read_html(URL)  # links are not present here so we need to parse the actual webpage
 
 # get the html code of the table from URL
 page = urllib.request.urlopen(URL).read()
@@ -25,28 +26,63 @@ textfilereader = pd.read_csv(f'https://www.ilo.org{file}', chunksize=2, nrows=6)
 # textfilereader is a generator object
 df = next(textfilereader)
 
-for file in all_csvgz_files[:2]:
+# classif1 --> id
+classif_id = {}
+
+# id ---> classif1
+id_classif = {}
+for file in all_csvgz_files:
     textfilereader = pd.read_csv(f'https://www.ilo.org{file}', chunksize=1, nrows=1)
     df = next(textfilereader)
-    '_'.join(df['classif1'][0].split('_')[:2])
+
+    if 'classif1' in df.keys():
+        k = '_'.join(df['classif1'][0].split('_')[:2])
+        v = file.split('/')[-1][:-len('.csv.gz')]
+
+        if k not in classif_id.keys():
+            classif_id[k] = [v]
+        else:
+            classif_id[k].append(v)
+
+        if v not in id_classif.keys():
+            id_classif[v] = [k]
+        else:
+            id_classif[v].append(k)
+
+    if 'classif2' in df.keys():
+        k = '_'.join(df['classif2'][0].split('_')[:2])
+        v = file.split('/')[-1][:-len('.csv.gz')]
+
+        if k not in classif_id.keys():
+            classif_id[k] = [v]
+        else:
+            classif_id[k].append(v)
+
+        if v not in id_classif.keys():
+            id_classif[v] = [k]
+        else:
+            id_classif[v].append(k)
+
+# writing the json file to with. 'With' is a context manager.
+with open("../data/results/classif_to_id.json", "w") as outfile:
+    json.dump(classif_id, outfile)
+
+with open("../data/results/id_to_classif.json", "w") as outfile:
+    json.dump(id_classif, outfile)
 
 
-# inverse mapping variable ---> dataframes
-d1 = {k:v} # k=id dataset, v:classiv
-d2 = {k:[] for k in unique(d1.values) + unique(d0.values)} # key is classif # d0.values=classif2
+pattern = 'https://www.ilo.org/ilostat-files/WEB_bulk_download/indicator/{id}.csv.gz'
+id_classif_href = {k: [pattern.format(id=v) for v in list_of_ids] for k, list_of_ids in id_classif.items()}
+classif_id_href = {k: [pattern.format(id=v) for v in list_of_ids] for k, list_of_ids in classif_id.items()}
 
-for k, v in d1:
-    d2[v].append(k)
+with open("../data/results/id_to_classif_href.json", "w") as outfile:
+    json.dump(id_classif_href, outfile)
+
+with open("../data/results/to_classif_id_href.json", "w") as outfile:
+    json.dump(classif_id_href, outfile)
 
 
 
-# (1) content based sorting
-
-# (2) create mapping df --> columnnames
-
-# table id!
-
-# (3) create mapping columnnames --> df
 
 
 
